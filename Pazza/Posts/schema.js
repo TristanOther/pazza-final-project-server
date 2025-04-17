1aa70c5e398131cdb3cbdb0bc899b25088dae999
TristanOther <tristan.n.lyons@gmail.com>
2025-04-16 23:15:53 -0400

    Added author and views fields to the posts schema.

--- Pazza/Posts/schema.js
+++ Pazza/Posts/schema.js
@@ -4,10 +4,12 @@ const postSchema = new mongoose.Schema({
     title:      { type: String,   required: true},
     content:    { type: String,   required: true},
     course:     { type: String,   required: true},
+    createdBy:  { type: String,   required: true},
     createdAt:  { type: Date,     required: true},
     instructor: { type: Boolean,  required: true,  default: false},
     tags:       { type: [String], required: true,  default: []},
     readBy:     { type: [String], required: true,  default: []},
+    viewedBy:   { type: [String], required: true,  default: []},
   }, 
   { collection: "pazza-posts" }
 );
