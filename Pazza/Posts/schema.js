d6c57f789d435809f77e3d260494995733093aa8
James Roth <roth.jame@northeastern.edu>
2025-04-17 15:52:49 -0400

    changed schema

--- Pazza/Posts/schema.js
+++ Pazza/Posts/schema.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 const postSchema = new mongoose.Schema({
     _id:        { type: String,   required: true},
     title:      { type: String,   required: true},
+    postType:   { type: String,   required: true},
     content:    { type: String,   required: true},
     course:     { type: String,   required: true},
     createdBy:  { type: String,   required: true},
@@ -10,6 +11,7 @@ const postSchema = new mongoose.Schema({
     tags:       { type: [String], required: true,  default: []},
     readBy:     { type: [String], required: true,  default: []},
     viewedBy:   { type: [String], required: true,  default: []},
+    viewableBy: { type: [String], required: true,  default: []},
   }, 
   { collection: "pazza-posts" }
 );
